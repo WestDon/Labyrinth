a7d6133c51cf2c22fb06f981982c0b6d48e0103c
Andrei Vaschuk <vashuk_andrei@mail.ru>
2017-03-22 22:50:29 +0200

    Revert "hhjhj"
    This reverts commit 5963c802d83293c107e686c62715556f22b3a8b2.

--- Labirint/LabWork.cs
+++ Labirint/LabWork.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace Labirint
 {
     public class LabWork
-    {rerer
+    {
         public int XCount { get; set; }
         public int YCount { get; set; }
         public int[,] ReadFile()
