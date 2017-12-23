449eae52d8bfe96dfd1337700af25001731cf208
Andrei Vaschuk <vashuk_andrei@mail.ru>
2017-12-23 12:16:16 +0200

    mrge

--- Labirint/LabWork.cs
+++ Labirint/LabWork.cs
@@ -1,7 +1,7 @@
-﻿using System;//some merge conflict
+﻿using System;//some merge conflictHY
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
